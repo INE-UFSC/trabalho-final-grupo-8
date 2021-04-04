2aaae229938159dbfbdeb3e7ad887bee855db9bf
felipeghizo <ghizoni71@gmail.com>
2021-04-03 22:35:50 -0300

    Menu atualizado mas nao completo

--- prototipo/game/menu.py
+++ prototipo/game/menu.py
@@ -1,51 +1,31 @@
-import PySimpleGUI as sg
+import pygame
+import pygame_menu
 
 
 def menu():
-    layout = [
-        [sg.Text(
-            'Bem vindo ao JOGO DA ORDENAÇÃO!!!',
-            background_color='black'
-        )],
+    pygame.init()
+    surface = pygame.display.set_mode((1280, 1024)) # Mexer na resolução.
 
-        [
-            sg.Text(
-                'Name: ', size=(15, 1), background_color='black'
-            ),
-            sg.InputText(background_color='gray')
-        ],
+    mytheme = pygame_menu.Theme(background_color=(29, 43, 82),
+                                title_background_color=(255, 255, 255),
+                                title_font_shadow=True,
+                                title_font=pygame_menu.font.FONT_8BIT,
+                                title_font_color=(29, 43, 82),
+                                widget_padding=25)
 
-        [
-            sg.Text('Selecione a dificuldade:', background_color='black'),
-            sg.InputCombo(
-                ("1", "2", "3", "4"),
-                background_color='gray', default_value="1"
-            )
-        ],
+    menu = pygame_menu.Menu("Jogo da Ordenacao", 800, 700, theme=mytheme)   # Mudar o nome do jogo.
 
-        [
-            sg.Text('Selecione o modo de jogo:', background_color='black'),
-            sg.Radio("tempo", "tempo", background_color='black'),
-            sg.Radio("movimentos", "movimentos", background_color='black')
-        ],
+    menu.add.text_input('Name :', font_color=(0, 0, 0))     # adicionar o player ao banco de dados.
+    menu.add.selector("dificuldade: ", [('Dificil', 3), ('Medio', 2), ('Facil', 1)], font_color=(0, 0, 0))
+    menu.add.selector("Modo de Jogo: ", [('tempo', 2), ('Movimentos', 1)], font_color=(0, 0, 0))
+    # menu.add.text_input('Array para ser ordenado :', font_color=(0, 0, 0))  definir o array
+    button1 = menu.add.button('Play', background_color=(40, 176, 255), font_color=(0, 0, 0))    # fazer o play funcionar.
+    button2 = menu.add.button('Quit', pygame_menu.events.EXIT, background_color=(255, 2, 76), font_color=(0, 0, 0))
 
-        [
-            sg.ReadFormButton(
-                "",
-                button_color='black',
-                image_filename="./assets/images.png",
-                image_size=(55, 80)
-            )
-        ]
-    ]
-
-    window = sg.Window(
-        'JOGO DA ORDENAÇÃO',
-        background_color='black'
-    ).Layout(layout)
-
-    event, values = window.Read()
-    sg.Popup(event, values)
+    button2.set_float(True)
+    button2.translate(100, 0)
+    button1.translate(-100, 0)
+    menu.mainloop(surface)
 
 
 if __name__ == '__main__':
