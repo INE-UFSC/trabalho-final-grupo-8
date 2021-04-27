c39769f93866f280025ab257838629d986abafee
felipeghizo <ghizoni71@gmail.com>
2021-04-27 10:40:49 -0300

    adiçao do BubbleSort e Selectionsort ao algorithm final.

--- versao_final/game/algorithm.py
+++ versao_final/game/algorithm.py
@@ -98,3 +98,102 @@ class Quicksort(Algorithm):
     def one_step(self):
         if self.__top >= 0:
             self.__aux()
+
+
+class Bubblesort(Algorithm):
+    """ Implementação do algoritmo de bubblesort:
+         O algoritmo percorre o array diversas vezes, e a cada passagem fazer flutuar para o topo o maior elemento da
+         sequencia."""
+
+    def __init__(self):
+        super().__init__()
+        self.__is_done = False
+        self.__total_higher = 0
+        self.__actual = 0
+        self.__next_position = 0
+        self.__less = 1
+
+    def is_done(self):
+        return self.__is_done
+
+    def sort_new(self, array: list):
+        self.__array = array
+        self.__is_done = False
+        self.__total_higher = 0
+        self.__actual = 0
+        self.__next_position = 0
+        self.__less = 1
+
+    def one_step(self):
+        ''' ordenacao de apenas um passo '''
+
+        for i in range(self.__next_position, len(self.__array) - 1):
+            if self.__array[i] > self.__array[i + 1]:
+                self.__array[i], self.__array[i+1] = self.__array[i+1], self.__array[i]     # Faz a troca de posicao.
+                self.__total_higher = 0               # Reset da variavel auxiliar.
+                self.__next_position = i + 1          # Posicao de partida do proximo looping e averiguacao de posicao.
+
+                # Caso o valor atinja sua posicao correta no array ordenado:
+                if self.__next_position == len(self.__array) - self.__less:
+                    self.__next_position = 0    # A variavel auxiliar de partida sofre reset.
+                    self.__less += 1   # E a posicao correta do proximo termo e uma anterior a do ultimo termo ordenado.
+                break
+
+            else:                           # Caso o proximo valor nao seja menor que o atual.
+                self.__total_higher += 1    # Variavel auxiliar conta quantas vezes isso ocorre.
+
+                # Caso seja igual ao comprimento da lista, o array esta ordenado:
+                if self.__total_higher == len(self.__array)-1:
+                    self.__is_done = True
+
+
+class Selectionsort(Algorithm):
+    """ Implementação do algoritmo de Selectionsort:
+        A ordenacao e feita de forma que o algoritmo procura o menor valor do array e o posiciona na primeira posicao,
+         trocando-o de lugar com o valor que ocupava tal posicao, entao a busca pelo segundo menor comeca e ao fim
+         posiciona o segundo menor valor na segunda posicao e assim por diante."""
+
+    def __init__(self):
+        super().__init__()
+        self.__is_done = False
+        self.__actual_less = self.__array[0]
+        self.__initial = 0
+        self.__higher = 0
+        self.__higher_true_position = 0
+
+    def is_done(self):
+        return self.__is_done
+
+    def sort_new(self, array: list):
+        self.__array = array
+        self.__is_done = False
+        self.__actual_less = self.__array[0]
+        self.__initial = 0
+        self.__higher = 0
+        self.__higher_true_position = 0
+
+    def one_step(self):
+        ''' ordenacao de apenas um passo '''
+
+        # Busca pelo menor valor partindo da posicao self.__initial:
+        for i in range(self.__initial, len(self.__array)):
+            # Caso o menor valor encontrado no array ate o momento seja maior que algum proximo:
+            if self.__actual_less > self.__array[i]:
+                self.__higher = self.__array[self.__initial]    # A variavel auxiliar armazena o maior valor.
+                self.__higher_true_position = i            # A variavel auxiliar armazena a posicao real do maior valor.
+                self.__actual_less = self.__array[i]        # A variavel auxiliar armazena o menor valor.
+
+        # E feita a troca de posicoes entre o menor e maior valores encontrados:
+        self.__array[self.__initial], self.__array[self.__higher_true_position] = self.__actual_less, self.__higher
+
+        # Como o menor valor ja esta em sua posicao, a proxima busca parte de uma casa adiante:
+        self.__initial += 1
+
+        # Caso self.__initial seja igual ao comprimento da lista, o array ja esta ordenado:
+        if self.__initial == len(self.__array):
+            self.__is_done = True
+
+        if self.__is_done is False:
+            # A variavel auxiliar responsavel por armazenar o menor valor encontrado ate o momento recebe o valor da
+            # primeira posicao da proxima busca:
+            self.__actual_less = self.__array[self.__initial]
