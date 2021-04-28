085d16fd87b880d61e73e31805c7a33e33e7ae5a
Vitor Silvestri <vitorsilvestri1@gmail.com>
2021-04-28 15:49:06 -0300

    refatoração do array

--- versao_final/game/__main__.py
+++ versao_final/game/__main__.py
@@ -1,12 +1,14 @@
 """ Módulo de entrada do projeto """
 
 
+from game.player import Player
 import random
 import pygame as pg
 from game.engine import DisplayManager, InputManager
-from game.numbers import InteractableNumberArray, NumberArray
+# from game.numbers import InteractableNumberArray, NumberArray
+from game.array import BoxFactory, Array
 from game.enemy import Enemy
-from game.algorithm import BubbleSort
+from game.algorithm import SelectionSort
 from game.ui import UI
 
 
@@ -23,25 +25,40 @@ def main():
     surface = pg.Surface((200, 150))
     gui = UI((200, 150))
 
-    array = [random.randint(0, 100) for _ in range(10)]
-    player_array = InteractableNumberArray(array, (0, 155, 0))
-    enemy_algorithm = BubbleSort()
-    enemy_array = NumberArray([], (255, 0, 0))
-    enemy = Enemy(enemy_algorithm, enemy_array, 1.0)
-    enemy.set_array(array)
+    blue_box = pg.image.load('./assets/BlueBox.png').convert_alpha()
+    red_box = pg.image.load('./assets/RedBox.png').convert_alpha()
+    font = pg.font.Font('./assets/OpenSansPXBold.ttf', 16)
+
+    player_array = Array(BoxFactory(blue_box, font), y_pos=134)
+    player = Player(player_array, inputs)
+
+    enemy = Array(BoxFactory(red_box, font), y_pos=0)
+
+    player_array.numbers = [0, 1, 2, 3, 4, 5]
+    enemy.numbers = [0, 1, 2, 3, 4, 5]
+
+    # array = [random.randint(0, 100) for _ in range(10)]
+    # player_array = InteractableNumberArray(array, (0, 155, 0))
+    # enemy_algorithm = SelectionSort()
+    # enemy_array = NumberArray([], (255, 0, 0))
+    # enemy = Enemy(enemy_algorithm, enemy_array, 1.0)
+    # enemy.set_array(array)
 
     while True:
         inputs.update([gui.handle_event])
         delta_time = display.tick()
-        surface.fill((0, 0, 0))
+        surface.fill((50, 50, 50))
 
         gui.update(delta_time)
 
         if gui.in_game():
-            enemy.update(delta_time)
-            player_array.handle_mouse(inputs)
-            enemy_array.draw(surface, 5, margin=5)
-            player_array.draw(surface, 130, margin=5)
+            player.update()
+            player_array.draw(surface)
+            enemy.draw(surface)
+            # enemy.update(delta_time)
+            # player_array.handle_mouse(inputs)
+            # enemy_array.draw(surface, 5, margin=5)
+            # player_array.draw(surface, 130, margin=5)
 
         gui.draw(surface)
         display.draw(surface)
---
+++ versao_final/game/array.py
@@ -0,0 +1,130 @@
+""" Módulos para as caixas de números """
+
+
+from typing import List, Tuple
+import pygame as pg
+
+
+class Box:
+    """ A caixa que contém um número """
+
+    def __init__(
+        self,
+        sprite: pg.Surface,
+        font: pg.font.Font,
+        number: int,
+    ):
+        self.__number = number
+        self.__sprite = sprite
+        self.__font = font
+        self.__rect = pg.Rect(0, 0, 16, 16)
+        self.__surface = pg.Surface((16, 16), pg.HWSURFACE | pg.SRCALPHA)
+        self.__dragged = False
+
+    @property
+    def rect(self):
+        """ A bounding box da caixa """
+        return self.__rect
+
+    @property
+    def surface(self):
+        """ A superfície da caixa """
+        return self.__surface
+
+    @property
+    def dragged(self):
+        """ Se a caixa está sendo arrastada """
+        return self.__dragged
+
+    @dragged.setter
+    def dragged(self, dragged: bool):
+        """ Altera o estado da caixa """
+        self.__dragged = dragged
+
+    def draw(self):
+        """ Desenha a caixa, podendo receber opcionalmente uma cor """
+        text = self.__font.render(str(self.__number), False, (0, 0, 0))
+        self.__surface.blit(self.__sprite, (0, 0))
+        self.__surface.blit(
+            text,
+            ((self.__rect.width - text.get_width()) / 2,
+             (self.__rect.height - text.get_height()) / 2)
+        )
+
+
+class BoxFactory:
+    """ Classe criadora de caixas """
+
+    def __init__(self, sprite: pg.Surface, font: pg.font.Font):
+        self.__sprite = sprite
+        self.__font = font
+
+    def create(self, number: int) -> Box:
+        """ Cria uma caixa conforme determinado número """
+        box = Box(self.__sprite, self.__font, number)
+        box.draw()
+        return box
+
+
+class Array:
+    """ Representação gráfica de um array """
+
+    def __init__(self, box_factory: BoxFactory, y_pos: int = 0):
+        self.__box_factory = box_factory
+        self.__numbers: list[Box] = []
+        self.__should_redraw = True
+        self.__y_pos = y_pos
+
+    @property
+    def numbers(self):
+        """ O array de caixas """
+        return self.__numbers
+
+    @numbers.setter
+    def numbers(self, array: List[int]):
+        self.__numbers = list(map(self.__box_factory.create, array))
+        self.__should_redraw = True
+
+    def __boxes_to_draw(self) -> List[Tuple[pg.Surface, pg.Rect]]:
+        """
+        Retorna uma lista de tuplas com a
+        superfície e a posição das caixas
+        """
+        boxes: List[Tuple[pg.Surface, pg.Rect]] = []
+        dragged = None
+        for box in self.__numbers:
+            if box.dragged:
+                dragged = box
+            else:
+                boxes.append((box.surface, box.rect))
+        if dragged is not None:
+            boxes.append((dragged.surface, dragged.rect))
+        return boxes
+
+    def __update_positions(self, width: int):
+        """ Corrige as posições das caixas conforme a largura e a altura """
+        if len(self.__numbers) == 0:
+            return
+        spacing = (
+            width - (len(self.__numbers) * self.__numbers[0].rect.width)
+        ) // (len(self.__numbers) - 1)
+        for index, box in enumerate(self.__numbers):
+            if not box.dragged:
+                box.rect.x = index * (box.rect.width + spacing)
+                box.rect.y = self.__y_pos
+
+    def draw(self, surface: pg.Surface):
+        """ Desenha o array na altura especificada """
+        if self.__should_redraw:
+            self.__update_positions(surface.get_width())
+        surface.blits(
+            self.__boxes_to_draw(),  # type: ignore
+            doreturn=False
+        )
+
+    def swap(self, origin: int, destination: int):
+        """ Troca a posição de duas caixas no array """
+        self.__should_redraw = True
+        (self.__numbers[origin], self.__numbers[destination]) = (
+            self.__numbers[destination], self.__numbers[origin]
+        )
